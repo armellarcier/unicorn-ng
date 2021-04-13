bbb1996a81e33fd92742ae8bd90ecb25f33efb45
Armel Larcier <armel.larcier@gmail.com>
2021-04-13 14:53:58 +0200

    Add icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
-import { UnicornCardComponent } from './unicorn-card/unicorn-card.component';
 import { CardComponent } from './card/card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UnicornCardComponent } from './unicorn-card/unicorn-card.component';
+import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-    declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, CardComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+    declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, CardComponent, NavComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatIconModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
