07c7e0f79afe706f4b948daf044bb208cbe3585d
Armel Larcier <armel.larcier@gmail.com>
2021-04-13 17:39:07 +0200

    Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ManageUnicornComponent } from './manage-unicorn/manage-unicorn.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { AgeFromBirthYearPipe } from './shared/pipes/age-from-birth-year.pipe';
 import { UnicornCardComponent } from './unicorn-card/unicorn-card.component';
@@ -25,6 +29,7 @@ import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
         CardComponent,
         NavComponent,
         AgeFromBirthYearPipe,
+        ManageUnicornComponent,
     ],
     imports: [
         BrowserModule,
@@ -38,6 +43,10 @@ import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
         MatSidenavModule,
         MatListModule,
         MatCardModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatInputModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/manage-unicorn/manage-unicorn.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+const CURRENT_YEAR = new Date().getFullYear();
+
+@Component({
+    selector: 'app-manage-unicorn',
+    templateUrl: './manage-unicorn.component.html',
+    styleUrls: ['./manage-unicorn.component.scss'],
+})
+export class ManageUnicornComponent implements OnInit {
+    public form = this.fb.group({
+        name: ['', [Validators.required]],
+        birthyear: [CURRENT_YEAR - 20, [Validators.required, Validators.min(1800), Validators.max(CURRENT_YEAR)]],
+    });
+    constructor(private fb: FormBuilder) {}
+
+    public save() {}
+
+    ngOnInit(): void {}
+}
