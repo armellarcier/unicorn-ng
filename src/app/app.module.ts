881e42dd78ccd8f92b1cf9c4a2cbfa70d5b01e8e
Armel Larcier <armel.larcier@gmail.com>
2021-04-13 14:54:33 +0200

    pretty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { UnicornCardComponent } from './unicorn-card/unicorn-card.component';
 import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
-import { NavComponent } from './shared/nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, CardComponent, NavComponent],
